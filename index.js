9d0acd049feb4799fb0d40cd2bfac9319045477d
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-06-22 11:20:49 +0530

    Added courses endpoint

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 require("dotenv").config();
-const express=require("express");
-const app=express();
-const PORT=process.env.PORT ;
-const admin_router=require("./backend/routes/admin-routes");
-const user_router=require("./backend/routes/user-routes")
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
+const admin_router = require("./backend/routes/admin-routes");
+const user_router = require("./backend/routes/user-routes");
+const course_router = require("./backend/routes/course-routes");
 
-console.log("ello from index")
-app.use("/api/v1/admin",admin_router);
+app.use("/api/v1/admin", admin_router);
 
-app.use("/api/v1/user",user_router);
+app.use("/api/v1/user", user_router);
 
+app.use("/api/v1/course", course_router);
 
-
-
-
-
-
-app.listen(PORT,()=>{
-    console.log(`Server is listening to ${PORT}......`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is listening to ${PORT}......`);
+});
